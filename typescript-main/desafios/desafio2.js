58ab68efcfb819f784e9c2f0f496833ffa46d625
Edward Cardoso <edwardcardoso560@gmail.com>
2022-06-30 18:11:48 -0300

    Add files via upload

---
+++ typescript-main/desafios/desafio2.js
@@ -0,0 +1,23 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {}
+    pessoa3.nome= "laura",
+    pessoa3.idade= "32",
+    pessoa3.profissao= "Atriz"
+;
+
+let pessoa4 = {}
+    pessoa4.nome = "carlos",
+    pessoa4.idade = 19,
+    pessoa4.profissao = "padeiro"
+;
---
+++ typescript-main/desafios/desafio3.js
@@ -0,0 +1,25 @@
+
+
+let botaoAtualizar = document.getElementById('atualizar-saldo');
+let botaoLimpar = document.getElementById('limpar-saldo');
+let soma = document.getElementById('soma');
+let campoSaldo = document.getElementById('campo-saldo');
+
+campoSaldo.innerHTML = 0
+
+function somarAoSaldo(soma) {
+    campoSaldo.innerHTML += soma;
+}
+
+function limparSaldo() {
+    campoSaldo.innerHTML = '';
+}
+
+botaoAtualizar.addEventListener('click', function () {
+    somarAoSaldo(soma.value);
+});
+
+botaoLimpar.addEventListener('click', function () {
+    limparSaldo();
+});
+
